83c5c98087b88f5599698587394b4f33935be70e
Marion BRUEL <90557768+marionbruel81@users.noreply.github.com>
2023-03-16 08:31:05 +0100

    Add accueil.php

---
+++ dolistats/views/accueil.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+	<head>
+		<meta charset='utf-8'>
+		<title>Accueil</title>
+		<link rel="stylesheet" href="../css/style.css"/>
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
+		<link rel="stylesheet" href="../fontawesome-free-5.10.2-web/css/all.css"/>
+	</head>
+	<body class="bodyPage">
+		<div class="container-fluid">
+			<!-- navbar -->
+			<div class="row navbar">
+				div class="col-md-1 col-xs-2">
+					<a href="pages/rechercheArticle.php">
+						<h2 class="titre-navbar">
+							<span class="fa fa-search fa-2x"></span><br/><br/>
+							Recherche Article 
+						</h2>
+					</a>
+				</div>
+
+				<div class="col-md-1 col-xs-2">
+					<a href="pages/rechercheClient.php">
+						<h2 class="titre-navbar">
+							<span class="fa fa-search fa-2x"></span><br/><br/>
+							Recherche Client 
+						</h2>
+					</a>
+				</div>
+
+				<div class="col-md-1 col-xs-2">
+					<a href="pages/palmaresArticle.php">
+						<h2 class="titre-navbar">
+							<span class="fa fa-chart-pie fa-2x"></span><br/><br/>
+							Palmarès Article 
+						</h2>
+					</a>
+				</div>
+
+				<div class="col-md-1 col-xs-2">
+					<a href="pages/palmaresClient.php">
+						<h2 class="titre-navbar">
+							<span class="fa fa-chart-pie fa-2x"></span><br/><br/>
+							Palmarès Client 
+						</h2>
+					</a>
+				</div>
+
+				<div class="col-md-1 col-xs-2">
+					<a href="pages/chiffreDAffaires.php">
+						<h2 class="titre-navbar">
+							<span class="fa fa-money-bill fa-2x"></span><br/><br/>
+							Chiffre d'affaires 
+						</h2>
+					</a>
+				</div>
+
+				<!-- case utilisateur en offset -->
+				<div class="col-md-1 col-md-offset-6 col-xs-2">
+					<h2 class="titre-navbar">
+						<span class="fa fa-user fa-2x"></span><br/><br/>
+						Déconnexion 
+					</h2>
+				</div>
+			</div>
+			<!-- Fin de la navbar -->
+			
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-sm-12">
+					<div class="bigDolistats">Dolistats</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
