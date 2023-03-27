132d9e02b379817d3feb891c40903b0eae30ccec
Wailtran08 <armand.mace@iut-rodez.fr>
2023-03-27 15:11:29 +0200

    Push RechercheArticleController.php

---
+++ dolistats/controllers/RechercheArticleController.php
@@ -0,0 +1,24 @@
+<?php
+namespace controllers;
+
+use yasmf\view;
+use yasmf\httpHelper;
+use services\APIService;
+
+class RechercheArticleController
+{
+    private APIService $apiService;
+
+    public function __construct(APIService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
+    /**
+     * @return view
+     */
+    public function index() : View {
+        return new view("views/recherche_article");
+    }
+}
+
